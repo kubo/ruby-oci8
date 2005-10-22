8db062d5a59cb78d891b8ea09110d5314421c2dd
Kubo Takehiro <kubo@jiubao.org>
2005-10-22 08:01:39 +0000

    * ext/oci8/oci8lib.c, ext/oci8/oci8.c, ext/oci8/svcctx.c, ext/oci8/extconf.rb:     oci8.c is renamed to oci8lib.c.     svcctx.c is renamed to oci8.c.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-oci8/trunk/ruby-oci8@91 f081da96-e817-0410-9439-ba585a54bbf1

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -1,150 +1,408 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
+  svcctx.c - part of ruby-oci8
+
+  Copyright (C) 2002-2005 KUBO Takehiro <kubo@jiubao.org>
+
 */
 #include "oci8.h"
 
-#define DEBUG_CORE_FILE 1
-#ifdef DEBUG_CORE_FILE
-#include <signal.h>
-#endif
+static VALUE cOCI8;
 
-oci8_base_class_t oci8_base_class = {
+enum logon_type_t {T_IMPLICIT, T_EXPLICIT};
+
+typedef struct  {
+    oci8_base_t base;
+    enum logon_type_t logon_type;
+    OCISession *authhp;
+    OCIServer *srvhp;
+} oci8_svcctx_t;
+
+static void oci8_svcctx_free(oci8_base_t *base)
+{
+    oci8_svcctx_t *svcctx = (oci8_svcctx_t *)base;
+    if (svcctx->authhp) {
+        OCIHandleFree(svcctx->authhp, OCI_HTYPE_SESSION);
+        svcctx->authhp = NULL;
+    }
+    if (svcctx->srvhp) {
+        OCIHandleFree(svcctx->srvhp, OCI_HTYPE_SERVER);
+        svcctx->srvhp = NULL;
+    }
+}
+
+static oci8_base_class_t oci8_svcctx_class = {
     NULL,
-    NULL,
-    sizeof(oci8_base_t),
+    oci8_svcctx_free,
+    sizeof(oci8_svcctx_t)
 };
 
-static ID id_oci8_class;
+static VALUE sym_SYSDBA;
+static VALUE sym_SYSOPER;
 
-static VALUE cOCI8Base;
-static VALUE mOCI8BindType;
-static VALUE cOCI8BindTypeBase;
-
-static VALUE oci8_handle_initialize(VALUE self)
+static VALUE oci8_svcctx_initialize(int argc, VALUE *argv, VALUE self)
 {
-    rb_raise(rb_eNameError, "private method `new' called for %s:Class", rb_class2name(CLASS_OF(self)));
+    VALUE vusername;
+    VALUE vpassword;
+    VALUE vdbname;
+    VALUE vmode;
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sword rv;
+
+    rb_scan_args(argc, argv, "31", &vusername, &vpassword, &vdbname, &vmode);
+
+    StringValue(vusername); /* 1 */
+    StringValue(vpassword); /* 2 */
+    if (!NIL_P(vdbname)) {
+        StringValue(vdbname); /* 3 */
+    }
+    if (NIL_P(vmode)) { /* 4 */
+#if defined(HAVE_OCISESSIONGET)
+#error TODO
+#elif defined(HAVE_OCILOGON2)
+#error TODO
+#else
+        rv = OCILogon(oci8_envhp, oci8_errhp, (OCISvcCtx **)&svcctx->base.hp,
+                      RSTRING(vusername)->ptr, RSTRING(vusername)->len,
+                      RSTRING(vpassword)->ptr, RSTRING(vpassword)->len,
+                      NIL_P(vdbname) ? NULL : RSTRING(vdbname)->ptr,
+                      NIL_P(vdbname) ? 0 : RSTRING(vdbname)->len);
+        if (rv != OCI_SUCCESS) {
+            oci8_raise(oci8_errhp, rv, NULL);
+        }
+#endif
+        svcctx->base.type = OCI_HTYPE_SVCCTX;
+        svcctx->logon_type = T_IMPLICIT;
+    } else {
+        ub4 mode;
+
+        Check_Type(vmode, T_SYMBOL);
+        if (vmode == sym_SYSDBA) {
+            mode = OCI_SYSDBA;
+        } else if (vmode == sym_SYSOPER) {
+            mode = OCI_SYSOPER;
+        } else {
+            rb_raise(rb_eArgError, "invalid privilege name %s (expect :SYSDBA or :SYSOPER)", rb_id2name(SYM2ID(vmode)));
+        }
+        /* allocate OCI handles. */
+        rv = OCIHandleAlloc(oci8_envhp, (void*)&svcctx->base.hp, OCI_HTYPE_SVCCTX, 0, 0);
+        if (rv != OCI_SUCCESS)
+            oci8_env_raise(oci8_envhp, rv);
+        svcctx->base.type = OCI_HTYPE_SVCCTX;
+        rv = OCIHandleAlloc(oci8_envhp, (void*)&svcctx->authhp, OCI_HTYPE_SESSION, 0, 0);
+        if (rv != OCI_SUCCESS)
+            oci8_env_raise(oci8_envhp, rv);
+        rv = OCIHandleAlloc(oci8_envhp, (void*)&svcctx->srvhp, OCI_HTYPE_SERVER, 0, 0);
+        if (rv != OCI_SUCCESS)
+            oci8_env_raise(oci8_envhp, rv);
+
+        /* set username and password to OCISession. */
+        rv = OCIAttrSet(svcctx->authhp, OCI_HTYPE_SESSION,
+                        RSTRING(vusername)->ptr, RSTRING(vusername)->len,
+                        OCI_ATTR_USERNAME, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        rv = OCIAttrSet(svcctx->authhp, OCI_HTYPE_SESSION,
+                        RSTRING(vpassword)->ptr, RSTRING(vpassword)->len,
+                        OCI_ATTR_PASSWORD, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+
+        /* attach to server and set to OCISvcCtx. */
+        rv = OCIServerAttach(svcctx->srvhp, oci8_errhp,
+                             NIL_P(vdbname) ? NULL : RSTRING(vdbname)->ptr,
+                             NIL_P(vdbname) ? 0 : RSTRING(vdbname)->len, OCI_DEFAULT);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        rv = OCIAttrSet(svcctx->base.hp, OCI_HTYPE_SVCCTX, svcctx->srvhp, 0, OCI_ATTR_SERVER, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+
+        /* attach to server and set to OCISvcCtx. */
+        rv = OCISessionBegin(svcctx->base.hp, oci8_errhp, svcctx->authhp, OCI_CRED_RDBMS, mode);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        rv = OCIAttrSet(svcctx->base.hp, OCI_HTYPE_SVCCTX, svcctx->authhp, 0, OCI_ATTR_SESSION, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        svcctx->logon_type = T_EXPLICIT;
+    }
+    return Qnil;
 }
 
-void oci8_base_free(oci8_base_t *base)
-{
-    if (base->klass->free != NULL)
-        base->klass->free(base);
-    if (base->type >= OCI_DTYPE_FIRST)
-        OCIDescriptorFree(base->hp, base->type);
-    else if (base->type >= OCI_HTYPE_FIRST)
-        OCIHandleFree(base->hp, base->type);
-    base->type = 0;
-    base->hp = NULL;
-}
+/*
+=begin
+--- OCISvcCtx#logoff()
+     disconnect from Oracle.
 
-static VALUE oci8_handle_free(VALUE self)
-{
-    oci8_base_t *base = DATA_PTR(self);
+     If you use ((<OCIServer#attach>)) and ((<OCISession#begin>)) to logon,
+     use ((<OCIServer#detach>)) and ((<OCISession#end>)) instead.
+     See also ((<Simplified Logon>)) and ((<Explicit Attach and Begin Session>)).
 
-    oci8_base_free(base);
+     correspond native OCI function: ((|OCILogoff|))
+=end
+*/
+static VALUE oci8_svcctx_logoff(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sword rv;
+
+    switch (svcctx->logon_type) {
+    case T_IMPLICIT:
+        rv = OCILogoff(svcctx->base.hp, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        svcctx->base.type = 0;
+        break;
+    case T_EXPLICIT:
+        rv = OCISessionEnd(svcctx->base.hp, oci8_errhp, svcctx->authhp, OCI_DEFAULT);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        rv = OCIServerDetach(svcctx->srvhp, oci8_errhp, OCI_DEFAULT);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+        break;
+    }
+    oci8_base_free(&svcctx->base);
     return self;
 }
 
-static void oci8_handle_mark(oci8_base_t *base)
+/*
+=begin
+--- OCISvcCtx#passwordChange(username, old_password, new_password [, mode])
+     :username
+        the username.
+     :old_password
+        old password of the user.
+     :new_password
+        new password of the user.
+     :mode
+        ((|OCI_DEFAULT|)) or ((|OCI_AUTH|)). Default value is ((|OCI_DEFAULT|)).
+
+        For most cases, use default value. If you want to know detail,
+        see "Oracle Call Interface Programmer's Guide".
+
+     correspond native OCI function: ((|OCIPasswordChange|))
+=end
+*/
+static VALUE oci8_password_change(VALUE self, VALUE username, VALUE opasswd, VALUE npasswd, VALUE mode)
 {
-    if (base->klass->mark != NULL)
-        base->klass->mark(base);
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sword rv;
+
+    StringValue(username); /* 1 */
+    StringValue(opasswd); /* 2 */
+    StringValue(npasswd); /* 3 */
+    Check_Type(mode, T_FIXNUM); /* 4 */
+
+    rv = OCIPasswordChange(svcctx->base.hp, oci8_errhp,
+                           RSTRING(username)->ptr, RSTRING(username)->len,
+                           RSTRING(opasswd)->ptr, RSTRING(opasswd)->len,
+                           RSTRING(npasswd)->ptr, RSTRING(npasswd)->len,
+                           FIX2INT(mode));
+    if (rv != OCI_SUCCESS) {
+        oci8_raise(oci8_errhp, rv, NULL);
+    }
+    return self;
 }
 
-static void oci8_handle_cleanup(oci8_base_t *base)
+/*
+=begin
+--- OCISvcCtx#commit([flags])
+     commit the transaction.
+
+     :flags
+        ((|OCI_DEFAULT|)) or ((|OCI_TRANS_TWOPHASE|)).
+        Default value is ((|OCI_DEFAULT|)).
+
+     correspond native OCI function: ((|OCITransCommit|))
+=end
+*/
+static VALUE oci8_trans_commit(int argc, VALUE *argv, VALUE self)
 {
-    oci8_base_free(base);
-    free(base);
+    VALUE vflags;
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    ub4 flags;
+    sword rv;
+
+    rb_scan_args(argc, argv, "01", &vflags);
+    Get_Int_With_Default(argc, 1, vflags, flags, OCI_DEFAULT); /* 1 */
+
+    rv = OCITransCommit(svcctx->base.hp, oci8_errhp, flags);
+    if (rv != OCI_SUCCESS) {
+        oci8_raise(oci8_errhp, rv, NULL);
+    }
+    return self;
 }
 
-static VALUE oci8_s_allocate(VALUE klass)
+/*
+=begin
+
+--- OCISvcCtx#rollback([flags])
+     rollback the transaction.
+
+     :flags
+        ((|OCI_DEFAULT|)) only valid. Default value is ((|OCI_DEFAULT|)).
+
+     correspond native OCI function: ((|OCITransRollback|))
+=end
+*/
+static VALUE oci8_trans_rollback(int argc, VALUE *argv, VALUE self)
+{
+    VALUE vflags;
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    ub4 flags;
+    sword rv;
+
+    rb_scan_args(argc, argv, "01", &vflags);
+    Get_Int_With_Default(argc, 1, vflags, flags, OCI_DEFAULT); /* 1 */
+
+    rv = OCITransRollback(svcctx->base.hp, oci8_errhp, flags);
+    if (rv != OCI_SUCCESS) {
+        oci8_raise(oci8_errhp, rv, NULL);
+    }
+    return self;
+}
+
+static VALUE oci8_svcctx_non_blocking_p(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sb1 non_blocking;
+    sword rv;
+
+    if (svcctx->srvhp == NULL) {
+        rv = OCIAttrGet(svcctx->base.hp, OCI_HTYPE_SVCCTX, &svcctx->srvhp, 0, OCI_ATTR_SERVER, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+    }
+    rv = OCIAttrGet(svcctx->srvhp, OCI_HTYPE_SERVER, &non_blocking, 0, OCI_ATTR_NONBLOCKING_MODE, oci8_errhp);
+    if (rv != OCI_SUCCESS)
+        oci8_raise(oci8_errhp, rv, NULL);
+    return non_blocking ? Qtrue : Qfalse;
+}
+
+static VALUE oci8_svcctx_set_non_blocking(VALUE self, VALUE val)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sb1 non_blocking;
+    sword rv;
+
+    if (svcctx->srvhp == NULL) {
+        rv = OCIAttrGet(svcctx->base.hp, OCI_HTYPE_SVCCTX, &svcctx->srvhp, 0, OCI_ATTR_SERVER, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+    }
+    rv = OCIAttrGet(svcctx->srvhp, OCI_HTYPE_SERVER, &non_blocking, 0, OCI_ATTR_NONBLOCKING_MODE, oci8_errhp);
+    if (rv != OCI_SUCCESS)
+        oci8_raise(oci8_errhp, rv, NULL);
+    if ((RTEST(val) && !non_blocking) || (!RTEST(val) && non_blocking)) {
+        /* toggle blocking / non-blocking. */
+        rv = OCIAttrSet(svcctx->srvhp, OCI_HTYPE_SERVER, 0, 0, OCI_ATTR_NONBLOCKING_MODE, oci8_errhp);
+        if (rv != OCI_SUCCESS)
+            oci8_raise(oci8_errhp, rv, NULL);
+    }
+    return val;
+}
+
+static VALUE oci8_server_version(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    OraText buf[1024];
+    sword rv;
+
+    rv = OCIServerVersion(svcctx->base.hp, oci8_errhp, buf, sizeof(buf), OCI_HTYPE_SVCCTX);
+    if (rv != OCI_SUCCESS)
+        oci8_raise(oci8_errhp, rv, NULL);
+    return rb_str_new2(buf);
+}
+
+#ifdef HAVE_OCISERVERRELEASE
+static VALUE oci8_server_release(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    OraText buf[1024];
+    ub4 version = 0;
+    sword rv;
+
+    rv = OCIServerRelease(svcctx->base.hp, oci8_errhp, buf, sizeof(buf), OCI_HTYPE_SVCCTX, &version);
+    if (rv != OCI_SUCCESS)
+        oci8_raise(oci8_errhp, rv, NULL);
+    return rb_ary_new3(2, INT2FIX(version), rb_str_new2(buf));
+}
+#endif
+
+static VALUE oci8_break(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sword rv;
+
+    rv = OCIBreak(svcctx->base.hp, oci8_errhp);
+    if (rv != OCI_SUCCESS)
+      oci8_raise(oci8_errhp, rv, NULL);
+    return self;
+}
+
+#ifdef HAVE_OCIRESET
+static VALUE oci8_reset(VALUE self)
+{
+    oci8_svcctx_t *svcctx = DATA_PTR(self);
+    sword rv;
+
+    rv = OCIReset(svcctx->base.hp, oci8_errhp);
+    if (rv != OCI_SUCCESS)
+      oci8_raise(oci8_errhp, rv, NULL);
+    return self;
+}
+#endif
+
+VALUE Init_oci8_svcctx(void)
+{
+    cOCI8 = oci8_define_class("OCI8", &oci8_svcctx_class);
+
+    sym_SYSDBA = ID2SYM(rb_intern("SYSDBA"));
+    sym_SYSOPER = ID2SYM(rb_intern("SYSOPER"));
+
+    rb_define_private_method(cOCI8, "__initialize", oci8_svcctx_initialize, -1);
+    rb_define_private_method(cOCI8, "__logoff", oci8_svcctx_logoff, 0);
+    rb_define_private_method(cOCI8, "__passwordChange", oci8_password_change, 4);
+    rb_define_private_method(cOCI8, "__commit", oci8_trans_commit, -1);
+    rb_define_private_method(cOCI8, "__rollback", oci8_trans_rollback, -1);
+    rb_define_private_method(cOCI8, "__non_blocking?", oci8_svcctx_non_blocking_p, 0);
+    rb_define_private_method(cOCI8, "__set_non_blocking", oci8_svcctx_set_non_blocking, 1);
+    rb_define_method(cOCI8, "version", oci8_server_version, 0);
+#ifdef HAVE_OCISERVERRELEASE
+    rb_define_method(cOCI8, "release", oci8_server_release, 0);
+#endif
+    rb_define_private_method(cOCI8, "__break", oci8_break, 0);
+#ifdef HAVE_OCIRESET
+    rb_define_private_method(cOCI8, "__reset", oci8_reset, 0);
+#endif
+
+    return cOCI8;
+}
+
+static oci8_svcctx_t *oci8_get_svcctx(VALUE obj)
 {
     oci8_base_t *base;
-    oci8_base_class_t *base_class;
-    VALUE superklass;
-    VALUE obj;
-
-    obj = rb_ivar_get(klass, id_oci8_class);
-    superklass = klass;
-    while (NIL_P(obj)) {
-        superklass = RCLASS(superklass)->super;
-	if (superklass == rb_cObject)
-	    rb_raise(rb_eRuntimeError, "private method `new' called for %s:Class", rb_class2name(klass));
-        obj = rb_ivar_get(superklass, id_oci8_class);
+    Check_Handle(obj, cOCI8, base);
+    if (base->type == 0) {
+        rb_raise(rb_eTypeError, "invalid argument %s was freed already.", rb_class2name(CLASS_OF(obj)));
     }
-    base_class = DATA_PTR(obj);
-
-    base = xmalloc(base_class->size);
-    memset(base, 0, base_class->size);
-
-    obj = Data_Wrap_Struct(klass, oci8_handle_mark, oci8_handle_cleanup, base);
-    base->self = obj;
-    base->klass = base_class;
-    return obj;
+    return (oci8_svcctx_t *)base;
 }
 
-void
-Init_oci8lib()
+OCISvcCtx *oci8_get_oci_svcctx(VALUE obj)
 {
-    VALUE cOCI8;
-
-    id_oci8_class = rb_intern("__oci8_class__");
-
-    Init_oci8_const();
-    Init_oci8_error();
-    Init_oci8_env();
-
-    /* OCI8 class */
-    cOCI8Base = rb_define_class("OCI8Base", rb_cObject);
-    cOCI8 = Init_oci8_svcctx();
-    mOCI8BindType = rb_define_module_under(cOCI8, "BindType");
-    cOCI8BindTypeBase = rb_define_class_under(mOCI8BindType, "Base", cOCI8Base);
-
-    rb_define_alloc_func(cOCI8Base, oci8_s_allocate);
-
-    rb_define_method(cOCI8Base, "initialize", oci8_handle_initialize, 0);
-    rb_define_method(cOCI8Base, "free", oci8_handle_free, 0);
-
-
-    /* Handle */
-    Init_oci8_bind(cOCI8BindTypeBase);
-    Init_oci8_stmt();
-
-    /* register allocators */
-    Init_oci8_rowid();
-    Init_oci8_param();
-    Init_oci8_lob(cOCI8);
-
-    Init_ora_date();
-    Init_ora_number();
-    Init_oci_number(cOCI8);
-    Init_oci_datetime();
-
-#ifdef DEBUG_CORE_FILE
-    signal(SIGSEGV, SIG_DFL);
-#endif
+    oci8_svcctx_t *svcctx = oci8_get_svcctx(obj);
+    return svcctx->base.hp;
 }
 
-VALUE oci8_define_class(const char *name, oci8_base_class_t *base_class)
+OCISession *oci8_get_oci_session(VALUE obj)
 {
-    VALUE klass = rb_define_class(name, cOCI8Base);
-    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, base_class);
-    rb_ivar_set(klass, id_oci8_class, obj);
-    return klass;
-}
+    oci8_svcctx_t *svcctx = oci8_get_svcctx(obj);
 
-VALUE oci8_define_class_under(VALUE outer, const char *name, oci8_base_class_t *base_class)
-{
-    VALUE klass = rb_define_class_under(outer, name, cOCI8Base);
-    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, base_class);
-    rb_ivar_set(klass, id_oci8_class, obj);
-    return klass;
-}
-
-VALUE oci8_define_bind_class(const char *name, oci8_bind_class_t *bind_class)
-{
-    VALUE klass = rb_define_class_under(mOCI8BindType, name, cOCI8BindTypeBase);
-    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, bind_class);
-    rb_ivar_set(klass, id_oci8_class, obj);
-    return klass;
+    if (svcctx->authhp == NULL) {
+        oci_lc(OCIAttrGet(svcctx->base.hp, OCI_HTYPE_SVCCTX, &svcctx->authhp, 0, OCI_ATTR_SESSION, oci8_errhp));
+    }
+    return svcctx->authhp;
 }
---
+++ ext/oci8/oci8lib.c
@@ -0,0 +1,150 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
+/*
+*/
+#include "oci8.h"
+
+#define DEBUG_CORE_FILE 1
+#ifdef DEBUG_CORE_FILE
+#include <signal.h>
+#endif
+
+oci8_base_class_t oci8_base_class = {
+    NULL,
+    NULL,
+    sizeof(oci8_base_t),
+};
+
+static ID id_oci8_class;
+
+static VALUE cOCI8Base;
+static VALUE mOCI8BindType;
+static VALUE cOCI8BindTypeBase;
+
+static VALUE oci8_handle_initialize(VALUE self)
+{
+    rb_raise(rb_eNameError, "private method `new' called for %s:Class", rb_class2name(CLASS_OF(self)));
+}
+
+void oci8_base_free(oci8_base_t *base)
+{
+    if (base->klass->free != NULL)
+        base->klass->free(base);
+    if (base->type >= OCI_DTYPE_FIRST)
+        OCIDescriptorFree(base->hp, base->type);
+    else if (base->type >= OCI_HTYPE_FIRST)
+        OCIHandleFree(base->hp, base->type);
+    base->type = 0;
+    base->hp = NULL;
+}
+
+static VALUE oci8_handle_free(VALUE self)
+{
+    oci8_base_t *base = DATA_PTR(self);
+
+    oci8_base_free(base);
+    return self;
+}
+
+static void oci8_handle_mark(oci8_base_t *base)
+{
+    if (base->klass->mark != NULL)
+        base->klass->mark(base);
+}
+
+static void oci8_handle_cleanup(oci8_base_t *base)
+{
+    oci8_base_free(base);
+    free(base);
+}
+
+static VALUE oci8_s_allocate(VALUE klass)
+{
+    oci8_base_t *base;
+    oci8_base_class_t *base_class;
+    VALUE superklass;
+    VALUE obj;
+
+    obj = rb_ivar_get(klass, id_oci8_class);
+    superklass = klass;
+    while (NIL_P(obj)) {
+        superklass = RCLASS(superklass)->super;
+        if (superklass == rb_cObject)
+            rb_raise(rb_eRuntimeError, "private method `new' called for %s:Class", rb_class2name(klass));
+        obj = rb_ivar_get(superklass, id_oci8_class);
+    }
+    base_class = DATA_PTR(obj);
+
+    base = xmalloc(base_class->size);
+    memset(base, 0, base_class->size);
+
+    obj = Data_Wrap_Struct(klass, oci8_handle_mark, oci8_handle_cleanup, base);
+    base->self = obj;
+    base->klass = base_class;
+    return obj;
+}
+
+void
+Init_oci8lib()
+{
+    VALUE cOCI8;
+
+    id_oci8_class = rb_intern("__oci8_class__");
+
+    Init_oci8_const();
+    Init_oci8_error();
+    Init_oci8_env();
+
+    /* OCI8 class */
+    cOCI8Base = rb_define_class("OCI8Base", rb_cObject);
+    cOCI8 = Init_oci8_svcctx();
+    mOCI8BindType = rb_define_module_under(cOCI8, "BindType");
+    cOCI8BindTypeBase = rb_define_class_under(mOCI8BindType, "Base", cOCI8Base);
+
+    rb_define_alloc_func(cOCI8Base, oci8_s_allocate);
+
+    rb_define_method(cOCI8Base, "initialize", oci8_handle_initialize, 0);
+    rb_define_method(cOCI8Base, "free", oci8_handle_free, 0);
+
+
+    /* Handle */
+    Init_oci8_bind(cOCI8BindTypeBase);
+    Init_oci8_stmt();
+
+    /* register allocators */
+    Init_oci8_rowid();
+    Init_oci8_param();
+    Init_oci8_lob(cOCI8);
+
+    Init_ora_date();
+    Init_ora_number();
+    Init_oci_number(cOCI8);
+    Init_oci_datetime();
+
+#ifdef DEBUG_CORE_FILE
+    signal(SIGSEGV, SIG_DFL);
+#endif
+}
+
+VALUE oci8_define_class(const char *name, oci8_base_class_t *base_class)
+{
+    VALUE klass = rb_define_class(name, cOCI8Base);
+    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, base_class);
+    rb_ivar_set(klass, id_oci8_class, obj);
+    return klass;
+}
+
+VALUE oci8_define_class_under(VALUE outer, const char *name, oci8_base_class_t *base_class)
+{
+    VALUE klass = rb_define_class_under(outer, name, cOCI8Base);
+    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, base_class);
+    rb_ivar_set(klass, id_oci8_class, obj);
+    return klass;
+}
+
+VALUE oci8_define_bind_class(const char *name, oci8_bind_class_t *bind_class)
+{
+    VALUE klass = rb_define_class_under(mOCI8BindType, name, cOCI8BindTypeBase);
+    VALUE obj = Data_Wrap_Struct(rb_cObject, 0, 0, bind_class);
+    rb_ivar_set(klass, id_oci8_class, obj);
+    return klass;
+}
