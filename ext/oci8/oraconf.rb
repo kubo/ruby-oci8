96804d7d05b33e910d231188352c94b1a2daa6a8
Kubo Takehiro <kubo@jiubao.org>
2006-08-20 14:12:17 +0000

    * ext/oci8/oraconf.rb: add error code for intel mac.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-oci8/branches/ruby-oci8-0.1@129 f081da96-e817-0410-9439-ba585a54bbf1

--- ext/oci8/oraconf.rb
+++ ext/oci8/oraconf.rb
@@ -160,6 +160,25 @@ class OraConf
       end
       return if try_link_oci()
 
+      if RUBY_PLATFORM =~ /darwin/
+        open('mkmf.log', 'r') do |f|
+          while line = f.gets
+            if line.include? 'cputype (18, architecture ppc) does not match cputype (7)'
+              raise <<EOS
+Oracle instant client is still pcc.
+  http://forums.oracle.com/forums/thread.jspa?messageID=1303271&#1303271
+
+There are three solutions:
+1. Compile ruby as ppc binary.
+2. Wait until Oracle releases mac intel binary.
+3. Use a third-party ODBC driver and ruby-odbc instead.
+     http://www.actualtechnologies.com/
+EOS
+            end
+          end
+        end
+      end
+
       raise 'cannot compile OCI'
     rescue
       print <<EOS
