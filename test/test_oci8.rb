07788126b023b056482b84b7cc7f5ddccb4072c2
Kubo Takehiro <kubo@jiubao.org>
2007-07-01 12:39:06 +0000

    * NEWS: add changes between 1.0.0-rc2 and 1.0.0-rc3. * VERSION: change version to 1.0.0-rc3. * ext/oci8/oraconf.rb: fix a problem to compile with a    third-party x86_64 rpm package. '--with-instant-client'    option become obsolete and is ignored. * test/test_oci8.rb: skip test_binary_float test when the testing    server version is 9i or earlier.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ruby-oci8/branches/ruby-oci8-1.0@193 f081da96-e817-0410-9439-ba585a54bbf1

--- test/test_oci8.rb
+++ test/test_oci8.rb
@@ -13,6 +13,14 @@ class TestOCI8 < RUNIT::TestCase
     @conn.logoff
   end
 
+  def server_version
+    retval = nil
+    @conn.exec('select value from database_compatible_level') do |row|
+      retval = row[0].split('.').map do |n| n.to_i; end
+    end
+    retval
+  end
+
   def test_select
     begin
       @conn.exec("DROP TABLE test_table")
@@ -141,6 +149,12 @@ EOS
   end
 
   def test_binary_float
+    if server_version[0] < 10
+      # Oracle 9i
+      print 'skip'
+      return
+    end
+    # Oracle 10g
     cursor = @conn.parse("select CAST(:1 AS BINARY_FLOAT), CAST(:2 AS BINARY_DOUBLE) from dual")
     cursor.bind_param(1, 10.0)
     cursor.bind_param(2, 100.1)
