5fca2bf6e3eff2a158f3c572b4e271335d3b657c
Kubo Takehiro <kubo@jiubao.org>
2022-03-14 07:24:03 +0900

    Remove code for old ruby versions

--- pre-distclean.rb
+++ pre-distclean.rb
@@ -1,6 +1,4 @@
-rm_f "#{curr_objdir}/lib/oci8.rb"
-rm_f "#{curr_objdir}/ext/oci8/oci8lib_18.map"
-rm_f "#{curr_objdir}/ext/oci8/oci8lib_191.map"
+rm_f "#{curr_objdir}/ext/oci8/oci8lib_*.map"
 if RUBY_PLATFORM =~ /cygwin/
   rm_f "#{curr_objdir}/ext/oci8/OCI.def"
   rm_f "#{curr_objdir}/ext/oci8/libOCI.a"
